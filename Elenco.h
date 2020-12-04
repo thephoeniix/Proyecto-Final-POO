e637453fdd42eae9d4bea8196ac9e866e349c78d
Karina Maldonado <73914621+thephoeniix@users.noreply.github.com>
2020-12-04 16:49:37 -0600

    Update Elenco.h

--- Elenco.h
+++ Elenco.h
@@ -1,29 +1,53 @@
+/*
+* ITESM Querétaro
+* Programación Orientada a Objetos
+* Proyecto
+* Karina Fernanda Maldonado Murillo A01707516
+* 04/12/2020
+*/
+
 #include <sstream>
 
 #include "Persona.h"
 using namespace std;
-
+/*
+* Declaro objeto Actor que hereda de Persona
+*/
 class Actor: public Persona{
     private:
+        //Variables de instancia del objeto
         string personaje;
 
     public:
-        Actor(): Persona(){
+         //Metodos del objeto
+    
+        //CONSTRUCTORES 
+        Actor(): Persona(){ //POR DEFAULT
             personaje = "";
         }
+        
+        //Constructor que recibe nombre y edad
         Actor(string nombre, int edad, string pers): Persona(nombre,edad){
             personaje = pers;
         }
-
+        
+        //Getter de Personaje
         string getPersonaje(){
             return personaje;
         };
-
+         
+        /*
+        * Se crea un metodo de tipo "void" para que se le pueda asignar un premio al Actor
+        * donde se utiliza el arreglo ya hecho y guardan ahí los datos.
+        */
+    
          void ganarPremio (string name, int year) {
             Premio newPremio(name, "Mejor actor", year);
             premios[numero_premios] = newPremio;
             numero_premios++;
          };
+    
+        //Metodo que imprime los Datos del Actor como Nombre, Edad y Personaje. 
 
          void printDatos() {
             cout<< "\n Actor: " << getName() << endl;
@@ -33,27 +57,38 @@ class Actor: public Persona{
 };
 
 
+//Declaro objeto Extra que hereda de Persona
 class Extra: public Persona{
+    //Variables de instancia del objeto
     private:
         int escena;
 
     public:
-        Extra(): Persona(){
+    //Metodo del objeto
+    
+    //CONSTRUCTORES 
+        Extra(): Persona(){ //DEFAULT
             escena = 0;
         }
-
+     //Constructor que recibe nombre y edad
         Extra(string nombre, int edad, int esc): Persona(nombre,edad){
             escena = esc;
         }
+        
+    //Getter de Escena.
         int getEscena(){
             return escena;
         };
-
+  
+     /*
+     * Se crea un metodo de tipo "void" para que se le pueda asignar un premio al Extra
+     * donde se utiliza el arreglo ya hecho y guardan ahí los datos.
+     */
          void ganarPremio () {
              cout<< "None";
          };
 
-
+    //Metodo que imprime datos del Extra como Nombre, Edad y Escena.
         void printDatos() {
             cout<< "\n Extra: " << getName() << endl;
             cout<< "Edad: " << getAge() << endl;
@@ -62,28 +97,43 @@ class Extra: public Persona{
 
 };
 
-
+//Declaro objeto Actor que hereda de Persona
 class Director: public Persona{
     private:
+        //Variables de instancia del objeto
         int salario;
 
     public:
-        Director(): Persona(){
+        //Metodos del objeto
+    
+        //CONSTRUCTORES 
+        Director(): Persona(){ //DEFAULT
             salario = 0;
         }
+        
+        //Constructor que recibe nombre y edad
         Director(string nombre, int edad, int sal): Persona(nombre,edad){
             salario = sal;
         }
-
+    
+        //Getter de Salario
         int getSalario(){
             return salario;
         };
+    
+        /*
+        * Se crea un metodo de tipo "void" para que se le pueda asignar un premio al Director
+        * donde se utiliza el arreglo ya hecho y guardan ahí los datos.
+        */
 
         void ganarPremio  (string name, int year) {
             Premio newPremio(name, "Mejor Director", year);
             premios[numero_premios] = newPremio;
             numero_premios++;
         };
+    
+       //Metodo que imprime los Datos del Director como Nombre, Edad y Salario. 
+
 
         void printDatos() {
             cout<< "\n Director: " << getName() << endl;
