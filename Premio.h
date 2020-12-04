ccc2420052fa3861145dde995922cad666300a6c
Karina Maldonado <73914621+thephoeniix@users.noreply.github.com>
2020-12-04 17:55:37 -0600

    Update Premio.h

--- Premio.h
+++ Premio.h
@@ -1,18 +1,35 @@
+/*
+* ITESM Querétaro
+* Programación Orientada a Objetos
+* Proyecto
+* Karina Fernanda Maldonado Murillo A01707516
+* 04/12/2020
+*/
+
 #include <sstream>
 
 using namespace std;
 
+/*
+* La clase Premio contiene métodos que guardan nombre, nominación y año
+*/
+
 class Premio {
     private:
+        //Declaro las variables de estancia
         string name;
         string nomination;
         int year;
     public:
-        Premio(): name(""), nomination(""), year(0){};
-        Premio(string n, string nom, int y): name(n), nomination(nom), year(y){};
+        //Constructor
+        Premio(): name(""), nomination(""), year(0){}; ///Default
+        Premio(string n, string nom, int y): name(n), nomination(nom), year(y){}; //Recibe valores de instancia
+    
+        //Getters
         string getName(){
             return name;
         };
+        
         string getNomination() {
             return nomination;
         };
